5eb8cc560cca8f731adceba01bd5bd8c74742fd2
Deepak Raj <rajvalmani@gmail.com>
2026-02-05 02:06:03 +0500

    Add files via upload

---
+++ week-01-introduction/examples/hello_world.cpp
@@ -0,0 +1,29 @@
+/**
+ * @file hello_world.cpp
+ * @brief Basic Hello World Program
+ * @author ITP Student
+ * @date 2024
+ * @course ITP C++ Programming - Week 1
+ * 
+ * This is the traditional first program in any programming language.
+ * It demonstrates the basic structure of a C++ program.
+ */
+
+#include <iostream>      // Include input/output stream library
+using namespace std;     // Use standard namespace
+
+/**
+ * @brief Main function - Program entry point
+ * 
+ * Every C++ program must have a main() function.
+ * This is where program execution begins.
+ * 
+ * @return int Exit status (0 indicates success)
+ */
+int main() {
+    // Output "Hello, World!" to the console
+    cout << "Hello, World!" << endl;
+    
+    // Return 0 to indicate successful execution
+    return 0;
+}
